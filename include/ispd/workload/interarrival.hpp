af6494b05a512784fb569b830aa43cdbf9c78bad
ricardofares <ricardoxdd28@outlook.com>
2023-08-13 09:35:56 -0300

    Added interarrival distribution abstraction.

---
+++ include/ispd/workload/interarrival.hpp
@@ -0,0 +1,27 @@
+#ifndef ISPD_WORKLOAD_INTERARRIVAL_HPP
+#define ISPD_WORKLOAD_INTERARRIVAL_HPP
+
+#include <ross.h>
+
+namespace ispd::workload {
+
+class InterarrivalDistribution {
+public:
+  virtual void generateInterarrival(tw_rng_stream *const rng, double &offset) = 0;
+  virtual void reverseGenerateInterarrival(tw_rng_stream *rng) = 0;
+};
+
+class FixedInterarrivalDistribution final : public InterarrivalDistribution {
+private:
+  double m_Interval;
+ 
+public:
+  explicit FixedInterarrivalDistribution(const double interval);
+
+  void generateInterarrival(tw_rng_stream *const rng, double &offset) override;
+  void reverseGenerateInterarrival(tw_rng_stream *const rng) override; 
+};
+
+} // namespace ispd::workload
+
+#endif // ISPD_WORKLOAD_INTERARRIVAL_HPP
---
+++ src/workload/interarrival.cpp
@@ -0,0 +1,29 @@
+#include <ispd/log/log.hpp>
+#include <ispd/workload/interarrival.hpp>
+
+namespace ispd::workload {
+
+FixedInterarrivalDistribution::FixedInterarrivalDistribution(const double interval) :
+    InterarrivalDistribution(), m_Interval(interval) {
+  /// Checks if the specified interval is not a number. If so, the program is immediately aborted.
+  if (std::isnan(interval))
+    ispd_error("Error in FixedInterarrivalDistribution constructor: The specified interarrival interval is not a valid number (NaN).");
+  
+  /// Checks if the specified interval is not finite. If so, the program is immediately aborted.
+  if (!std::isfinite(interval))
+    ispd_error("Error in FixedInterarrivalDistribution constructor: The specified interarrival interval must be finite.");
+
+  /// Checks if the specified interval is non-positive. If so, the program is immediately aborted.
+  if (interval <= 0.0)
+    ispd_error("Error in FixedInterarrivalDistribution constructor: The specified interarrival is non-positive.");
+}
+
+void FixedInterarrivalDistribution::generateInterarrival(tw_rng_stream *const rng, double &offset) {
+  offset = m_Interval;
+}
+
+void FixedInterarrivalDistribution::reverseGenerateInterarrival([[maybe_unused]] tw_rng_stream *const rng) {
+  /// There is no reverse action to be taken in fixed interarrival distribution.
+}
+
+} // namespace ispd::workload
