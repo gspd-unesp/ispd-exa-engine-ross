17528f89156a738bc8b30aa20e63aa6a217e8234
ricardofares <ricardoxdd28@outlook.com>
2023-08-15 11:50:23 -0300

    Added `UserMetrics` inside `User` structure

---
+++ include/ispd/metrics/user_metrics.hpp
@@ -0,0 +1,38 @@
+#ifndef ISPD_METRICS_USER_HPP
+#define ISPD_METRICS_USER_HPP
+
+namespace ispd::metrics {
+
+/// \struct UserMetrics
+///
+/// \brief A structure to hold various metrics related to a user's activities.
+///
+/// This structure is used to store different metrics associated with a user's
+/// activities within a system. It provides a convenient way to track and manage
+/// metrics related to processing, communication, energy consumption, and
+/// workload for a specific user.
+///
+struct UserMetrics final {
+  /// \name Processing-related metrics
+  double m_ProcTime;        ///< The total processing time used by the user (in seconds).
+  double m_ProcWaitingTime; ///< The waiting time for processing tasks (in
+                            ///< seconds).
+
+  /// \name Communication-related metrics
+  double m_CommTime;        ///< The total communication time used by the user (in
+                            ///< seconds).
+  double m_CommWaitingTime; ///< The waiting time for communication tasks (in
+                            ///< seconds).
+
+  /// \name Energy-related metrics
+  double m_EnergyConsumption; ///< The total energy consumption by the user (in
+                              ///< kJ).
+
+  /// \name Workload-related metrics
+  unsigned m_IssuedTasks;    ///< The total number of tasks issued by the user.
+  unsigned m_CompletedTasks; ///< The total number of tasks completed by the user.
+};
+
+} // namespace ispd::metrics
+
+#endif // ISPD_METRICS_USER_HPP
--- include/ispd/model/user.hpp
+++ include/ispd/model/user.hpp
@@ -1,6 +1,8 @@
 #ifndef ISPD_MODEL_USER_HPP
 #define ISPD_MODEL_USER_HPP
 
+#include <ispd/metrics/user_metrics.hpp>
+
 namespace ispd::model {
 
 /// \class User
@@ -19,10 +21,8 @@ public:
   /// \param name The name of the user.
   /// \param energyConsumptionLimit The energy consumption limit of the user
   /// (default: 0.0).
-  explicit User(const uid_t id, const std::string &name,
-                const double energyConsumptionLimit = 0.0)
-      : m_Id(id), m_Name(name),
-        m_EnergyConsumptionLimit(energyConsumptionLimit) {}
+  explicit User(const uid_t id, const std::string &name, const double energyConsumptionLimit = 0.0)
+      : m_Id(id), m_Name(name), m_EnergyConsumptionLimit(energyConsumptionLimit) {}
 
   /// \brief Get the unique identifier of the user.
   ///
@@ -34,6 +34,16 @@ public:
   /// \return The name associated with the user.
   inline const std::string &getName() const { return m_Name; }
 
+  /// \brief Retrieve the node's view of user metrics associated with this instance.
+  ///
+  /// This member function allows access to the `UserMetrics` object associated
+  /// with the current instance. Users can use this function to retrieve and
+  /// manipulate the metrics related to their activities within the system.
+  ///
+  /// \return A reference to the `UserMetrics` object storing the user's metrics.
+  ///
+  inline ispd::metrics::UserMetrics &getMetrics() { return m_Metrics; }
+
   /// \brief Get the energy consumption limit of the user.
   ///
   /// \return The energy consumption limit set for the user.
@@ -42,6 +52,7 @@ public:
 private:
   uid_t m_Id;                            ///< The user's unique identifier.
   std::string m_Name;                    ///< The user's name.
+  ispd::metrics::UserMetrics m_Metrics;  ///< The node's view of user's metrics.
   double m_EnergyConsumptionLimit = 0.0; ///< The energy consumption limit.
 };
 
