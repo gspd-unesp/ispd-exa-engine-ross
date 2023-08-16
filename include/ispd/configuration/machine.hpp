c2d5705c83b0b82e6b10ef99500baf854ff22bcc
ricardofares <ricardoxdd28@outlook.com>
2023-08-16 09:38:31 -0300

    Added machine power consumption getter member-functions with its respective documentation

--- include/ispd/configuration/machine.hpp
+++ include/ispd/configuration/machine.hpp
@@ -65,6 +65,42 @@ public:
   ///
   /// \return Load factor of the machine (0.0 to 1.0).
   inline double getLoad() const { return m_Load; }
+
+  /// \brief Retrieves the idle power consumption of the machine.
+  ///
+  /// This function returns the power consumption of the machine when it is
+  /// idle, measured in watts (W). Idle power consumption refers to the amount
+  /// of power consumed by the machine's components when they are not actively
+  /// processing tasks or performing any computational work. It provides insight
+  /// into the baseline power usage of the machine under minimal or no load
+  /// conditions.
+  ///
+  /// \return Idle power consumption of the machine in watts (W).
+  inline double getWattageIdle() const { return m_WattageIdle; }
+
+  /// \brief Retrieves the maximum power consumption of the machine.
+  ///
+  /// This function returns the maximum power consumption of the machine,
+  /// measured in watts (W). The maximum power consumption represents the
+  /// highest amount of power that the machine's components can consume when
+  /// running at full capacity or under heavy load. Understanding the maximum
+  /// power consumption is crucial for estimating power supply requirements and
+  /// managing energy usage in resource-intensive scenarios.
+  ///
+  /// \return Maximum power consumption of the machine in watts (W).
+  inline double getWattageMax() const { return m_WattageMax; }
+
+  /// \brief Retrieves the power consumption per core of the machine.
+  ///
+  /// This function returns the power consumption per core of the machine,
+  /// measured in watts (W). It indicates the amount of power consumed by each
+  /// individual processing core or unit within the machine. The power
+  /// consumption per core is a valuable metric for optimizing power efficiency
+  /// and workload distribution, as it helps in balancing computational tasks
+  /// across cores to achieve better energy utilization.
+  ///
+  /// \return Power consumption per core of the machine in watts (W).
+  inline double getWattagePerCore() const { return m_WattagePerCore; }
 };
 
 } // namespace ispd::configuration
