b3304bae9a7614476b12295c4803c12dc2c9be27
ricardofares <ricardoxdd28@outlook.com>
2023-08-16 09:35:29 -0300

    Added wattage information in `MachineConfiguration`

--- include/ispd/configuration/machine.hpp
+++ include/ispd/configuration/machine.hpp
@@ -6,40 +6,52 @@ namespace ispd::configuration {
 ///
 /// \brief Represents the configuration of a machine in the simulation.
 ///
-/// The MachineConfiguration class encapsulates various parameters related to the
-/// configuration of a machine in the simulation environment. It provides methods
-/// to calculate processing time and obtain power-related information based on the
-/// specified parameters.
+/// The MachineConfiguration class encapsulates various parameters related to
+/// the configuration of a machine in the simulation environment. It provides
+/// methods to calculate processing time and obtain power-related information
+/// based on the specified parameters.
 class MachineConfiguration final {
 private:
   double m_PowerPerCore; ///< Computational power per core (in megaflops).
   double m_Load;         ///< Load factor of the machine (0.0 to 1.0).
   unsigned m_CoreCount;  ///< Number of cores in the machine.
 
+  double m_WattageIdle; ///< Power consumption (in watts) being idle.
+  double
+      m_WattageMax; ///< Power consumpttion (in watts) at maximum utilization.
+  double m_WattagePerCore; ///< Average power consumption (in watts) per core.
 public:
   /// \brief Constructor for MachineConfiguration.
   ///
-  /// Initializes a new instance of the MachineConfiguration class with the provided
-  /// parameters.
+  /// Initializes a new instance of the MachineConfiguration class with the
+  /// provided parameters.
   ///
   /// \param power Computational power of the machine (in megaflops).
   /// \param load Load factor of the machine (0.0 to 1.0).
   /// \param coreCount Number of cores in the machine.
-  MachineConfiguration(const double power, const double load, const unsigned coreCount)
-    : m_PowerPerCore(power / coreCount), m_Load(load), m_CoreCount(coreCount) {}
+  explicit MachineConfiguration(const double power, const double load,
+                                const unsigned coreCount,
+                                const double wattageIdle = 0.0,
+                                const double wattageMax = 0.0)
+      : m_PowerPerCore(power / coreCount), m_Load(load), m_CoreCount(coreCount),
+        m_WattageIdle(wattageIdle), m_WattageMax(wattageMax),
+        m_WattagePerCore((wattageMax - wattageIdle) / coreCount) {}
 
   /// \brief Calculates the time required to process a task.
   ///
-  /// Calculates and returns the time required to process a workload of the given size
-  /// based on the machine's load and computational power per core.
+  /// Calculates and returns the time required to process a workload of the
+  /// given size based on the machine's load and computational power per core.
   ///
-  /// \param processingSize Processing size of the task to be processed (in megaflop).
+  /// \param processingSize Processing size of the task to be processed (in
+  ///                       megaflop).
+  ///
   /// \return Time required to process the task (in seconds).
   inline double timeToProcess(const double processingSize) {
     return processingSize / ((1.0 - m_Load) * m_PowerPerCore);
   }
 
-  /// \brief Returns the total computational power (in megaflops) of the machine.
+  /// \brief Returns the total computational power (in megaflops) of the
+  ///        machine.
   ///
   /// \return Total computational power of the machine (in megaflops).
   inline double getPower() const { return m_PowerPerCore * m_CoreCount; }
