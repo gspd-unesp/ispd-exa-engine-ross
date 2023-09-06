e7fe883579043e022ec8f70bf272d6634e6b66d7
ricardofares <ricardoxdd28@outlook.com>
2023-09-06 10:32:20 -0300

    Added the `TwoStageUniform` workload parameter generator

--- src/workload/workload.cpp
+++ src/workload/workload.cpp
@@ -82,6 +82,75 @@ namespace ispd::workload {
              remainingTasks);
 }
 
+[[nodiscard]] TwoStageUniformWorkload::TwoStageUniformWorkload(
+    const std::string &user, const unsigned remainingTasks,
+    const double computingOffload, const TwoStageDistribution procDist,
+    const TwoStageDistribution commDist,
+    std::unique_ptr<InterarrivalDistribution> interarrivalDist) noexcept
+    : Workload(user, remainingTasks, computingOffload,
+               std::move(interarrivalDist)),
+      m_ProcDist(procDist), m_CommDist(commDist) {
+
+  const auto minProcSize = std::get<TwoStageDistSelector::MINIMUM>(procDist);
+  const auto medProcSize = std::get<TwoStageDistSelector::MEDIUM>(procDist);
+  const auto maxProcSize = std::get<TwoStageDistSelector::MAXIMUM>(procDist);
+  const auto probProcStage =
+      std::get<TwoStageDistSelector::PROBABILITY>(procDist);
+
+  const auto minCommSize = std::get<TwoStageDistSelector::MINIMUM>(commDist);
+  const auto medCommSize = std::get<TwoStageDistSelector::MEDIUM>(commDist);
+  const auto maxCommSize = std::get<TwoStageDistSelector::MAXIMUM>(commDist);
+  const auto probCommStage =
+      std::get<TwoStageDistSelector::PROBABILITY>(commDist);
+
+  if (minProcSize <= 0.0)
+    ispd_error("Minimum processing size must be positive (Specified "
+               "minimum processing size: %lf).",
+               minProcSize);
+
+  if (medProcSize <= 0.0)
+    ispd_error("Medium processing size must be positive (Specified medium "
+               "processing size: %lf).",
+               medProcSize);
+
+  if (maxProcSize <= 0.0)
+    ispd_error("Maximum processing size must be positive (Specified "
+               "maximum processing size: %lf).",
+               maxProcSize);
+
+  if (probProcStage < 0.0 || probProcStage > 1.0)
+    ispd_error(
+        "Processing stage selection probability must be in the interval [0, "
+        "1]. (Specified processing stage selection probability: %lf).",
+        probProcStage);
+
+  if (minCommSize <= 0.0)
+    ispd_error("Minimum communication size must be positive (Specified "
+               "minimum communication size: %lf).",
+               minCommSize);
+
+  if (medCommSize <= 0.0)
+    ispd_error("Medium communication size must be positive (Specified medium "
+               "communication size: %lf).",
+               medCommSize);
+
+  if (maxCommSize <= 0.0)
+    ispd_error("Maximum communication size must be positive (Specified "
+               "maximum communication size: %lf).",
+               maxCommSize);
+
+  if (probCommStage < 0.0 || probCommStage > 1.0)
+    ispd_error(
+        "Communication stage selection probability must be in the interval [0, "
+        "1]. (Specified communication stage selection probability: %lf).",
+        probCommStage);
+
+  ispd_debug("[TwoStageUniform Workload] PI: [%lf, %lf, %lf, %lf], CI: [%lf, %lf, %lf, "
+             "%lf], RT: %u.",
+             minProcSize, medProcSize, maxProcSize, probProcStage, minCommSize,
+             medCommSize, maxCommSize, probCommStage, remainingTasks);
+}
+
 [[nodiscard]] NullWorkload::NullWorkload(const std::string &user) noexcept
     : Workload(user, 0, 0, nullptr) {}
 
@@ -104,6 +173,16 @@ uniform(const std::string &user, const unsigned remainingTasks,
   return new UniformWorkload(user, remainingTasks, minProcSize, maxProcSize,
                              minCommSize, maxCommSize, computingOffload,
                              std::move(interarrivalDist));
+}
+
+TwoStageUniformWorkload *
+twoStage(const std::string &user, const unsigned remainingTasks,
+         const double computingOffload, const TwoStageDistribution procDist,
+         const TwoStageDistribution commDist,
+         std::unique_ptr<InterarrivalDistribution> interarrivalDist) {
+  return new TwoStageUniformWorkload(user, remainingTasks, computingOffload,
+                                     procDist, commDist,
+                                     std::move(interarrivalDist));
 }
 
 NullWorkload *null(const std::string &user) { return new NullWorkload(user); }
